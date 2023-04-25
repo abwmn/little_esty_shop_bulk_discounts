4a105865deb714304c585ea78784275dbdeb7d42
Andrew Bowman <andrewtbowman@gmail.com>
2023-04-25 16:48:27 -0400

    test: sad path for discount update

--- spec/features/discounts/edit_spec.rb
+++ spec/features/discounts/edit_spec.rb
@@ -22,8 +22,27 @@ RSpec.describe 'Merchant Bulk Discount Edit' do
     click_button 'Update Discount'
 
     expect(current_path).to eq(merchant_discount_path(@merchant, @discount))
-
     expect(page).to have_content("Percentage Discount: 15%")
     expect(page).to have_content("Quantity Threshold: 8")
+
+    click_link 'Edit Discount'
+
+    expect(current_path).to eq(edit_merchant_discount_path(@merchant, @discount))
+
+    fill_in 'Percentage Discount (%)', with: -15
+    fill_in 'Quantity Threshold', with: -8
+    click_button 'Update Discount'
+
+    expect(current_path).to eq(merchant_discount_path(@merchant, @discount))
+    expect(page).to have_content('Percent must be greater than 0')
+    expect(page).to have_content('Quantity must be greater than 0')
+    expect(page).not_to have_link('Edit Discount')
+
+    fill_in 'Percentage Discount (%)', with: 20
+    fill_in 'Quantity Threshold', with: 10
+    click_button 'Update Discount'
+
+    expect(current_path).to eq(merchant_discount_path(@merchant, @discount))
+    expect(page).to have_link('Edit Discount')
   end
 end
