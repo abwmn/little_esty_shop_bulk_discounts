374e3d8e91e830a6ba662f16ac120f1eddd6b0ad
Andrew Bowman <andrewtbowman@gmail.com>
2023-04-25 15:09:59 -0400

    feat: us6 merchant total, discounted revenue

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -13,4 +13,16 @@ class Invoice < ApplicationRecord
   def total_revenue
     invoice_items.sum("unit_price * quantity")
   end
+
+  def merchant_total_revenue(merchant_id)
+    invoice_items
+    .joins(:item)
+    .where(items: { merchant_id: merchant_id })
+    .sum('invoice_items.quantity * invoice_items.unit_price')
+  end
+
+  def merchant_discounted_revenue(merchant_id)
+    merchant_invoice_items = invoice_items.joins(:item).where(items: { merchant_id: merchant_id })
+    merchant_invoice_items.sum { |invoice_item| invoice_item.discounted_revenue }
+  end
 end
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -14,4 +14,23 @@ class InvoiceItem < ApplicationRecord
     invoice_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:invoice_id)
     Invoice.order(created_at: :asc).find(invoice_ids)
   end
+
+
+  def discounted_revenue
+    discount = applicable_discount
+    if discount
+      discount_percentage = discount.percent.to_f / 100.0
+      discounted_price = unit_price * (1 - discount_percentage)
+      quantity * discounted_price
+    else
+      quantity * unit_price
+    end
+  end
+
+  def applicable_discount
+    item.merchant.discounts
+        .where('quantity <= ?', quantity)
+        .order(percent: :desc)
+        .first
+  end
 end
